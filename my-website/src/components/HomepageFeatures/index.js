15db5039c092370edfc7efe87b85d6eeb5602e7f
smichalski17-dev <smichalski17@gmail.com>
2025-11-24 13:15:17 -0600

    error corrected

--- my-website/src/components/HomepageFeatures/index.js
+++ my-website/src/components/HomepageFeatures/index.js
@@ -36,7 +36,7 @@ function Feature({Svg, title, description}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureJpg
+        <Svg className={styles.featureSvg
           
         } role="img" />
       </div>
